a06aab697b8efb0f139f6f6e0179918d68b0c977
irynajevaga <irynajevaga@gmail.com>
2024-04-27 10:21:58 +0200

    my commit add done

--- ping.sh
+++ ping.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-target_address="google.com"
-max_ping_time_ms=100
-max_failures=3
+target_address=$1
+max_ping_time_ms=$2
+max_failures=$3
 failures=0
 
 while true; do
   ping_result=$(ping -c 1 "$target_address" | grep icmp_seq)
-  ping_time=$(echo "$ping_result" | awk -F"=" {print $4} | awk '{print $1}')
+  ping_time=$(echo "$ping_result" | awk -F'=' {print $4} | awk '{print $1}')
 
   if [ -n "$ping_time" ] && [ "$ping_time" -gt "$max_ping_time_ms" ]; then
     echo "Пинг $target_address превышает $max_ping_time_ms мс: $ping_time мс."
@@ -24,4 +24,5 @@ while true; do
     failures=0
     break
   fi
+  sleep 1
 done
